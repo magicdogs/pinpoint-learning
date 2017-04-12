22c11eba2392a3fb0aeb8da3a91e80962705acc3
magicdog <80099856@qq.com>
2017-04-12 14:33:52 +0800

    'update'

---
+++ src/main/java/com/magic/example/Example1.java
@@ -0,0 +1,10 @@
+package com.magic.example;
+
+/**
+ * Created by magicdog on 2017/4/12.
+ */
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
